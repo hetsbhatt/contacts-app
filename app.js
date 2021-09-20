e344b88d15c97502bb5606349943990c5c1da63c
Het Bhatt <61598332+hetsbhatt@users.noreply.github.com>
2021-09-20 16:05:14 -0400

    contacts app v2

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 
-let items = [];
+let contactsArray = [];
+let newContact = [];
+// let photos = [];
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    //res.send("Hello");
-    const day = date.getDate();
-    res.render("list", { kindOfDay: day, newItems: items });
-});
 
-app.get("/about", (req, res) => {
-    res.render("about");
+
+app.get("/", (req, res) => {
+
+    res.render("contacts", { contactArr: contactsArray });
 });
 
 app.post("/", (req, res) => {
-
-    const newToDoEntry = req.body.toDoItem;
-    items.push(newToDoEntry);
+    //const newPhoto = req.body.photo;
+    const newFirstName = req.body.fName;
+    const newLastName = req.body.lName;
+    const newEmail = req.body.email;
+    const newNumber = req.body.phone;
+    //photo.push(newPhoto);
+    newContact.push(newFirstName, newLastName, newEmail, newNumber);
+    contactsArray.push(newContact);
     res.redirect("/");
+    //console.log(newPhoto);
+
+
 })
 
 app.listen(process.env.PORT || 3000, () => {
---
+++ views/delete.js
@@ -0,0 +1,7 @@
+function removeContact() {
+
+    if (this.visibility == 'visible') {
+        this.style.visibility = 'hidden';
+    } else
+        node.style.visibility = 'visible'
+}
